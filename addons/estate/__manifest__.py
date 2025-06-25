df69b721275d9bf46ae60afef66abc208764b2e4
Virgiawan Listiyandi <listiyandiv@gmail.com>
2025-06-25 13:16:47 +0700

    feat: basic security, menu and actions

--- addons/estate/__manifest__.py
+++ addons/estate/__manifest__.py
@@ -3,4 +3,9 @@
   'depends': ['base'],
   'application': True,
   'installable': True,
+  'data': [
+    'views/estate_property_views.xml',
+    'views/estate_menus.xml',
+    'security/ir.model.access.csv',
+  ],
 }
