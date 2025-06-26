e1ce073925df29466c89be203e3bf31a2c4dc9ca
Virgiawan Listiyandi <listiyandiv@gmail.com>
2025-06-26 14:14:20 +0700

    feat: define model relations for estate property

--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -41,3 +41,18 @@ class EstateProperty(models.Model):
         default="new",
         copy=False,
     )
+    property_type_id = fields.Many2one("estate.property.type", string="Property Type")
+    salesperson_id = fields.Many2one(
+        "res.users",
+        string="Salesman",
+        index=True,
+        default=lambda self: self.env.user,
+    )
+    buyer_id = fields.Many2one(
+        "res.partner",
+        string="Buyer",
+        index=True,
+        copy=False,
+    )
+    tag_ids = fields.Many2many("estate.property.tag", string="Tags")
+    offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
