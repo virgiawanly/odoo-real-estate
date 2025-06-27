588c229acc6145d23273d49d69c4a9446e74b77a
Virgiawan Listiyandi <listiyandiv@gmail.com>
2025-06-27 14:56:38 +0700

    feat: add sql and python validation constraints

--- addons/estate/models/estate_property_type.py
+++ addons/estate/models/estate_property_type.py
@@ -1,7 +1,12 @@
 from odoo import fields, models
 
+
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
     _description = "Estate Property Type"
-    
+
     name = fields.Char("Name", required=True)
+
+    _sql_constraints = [
+        ("check_type_name_unique", "UNIQUE(name)", "Type name must be unique.")
+    ]
